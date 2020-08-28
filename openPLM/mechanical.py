51176aa7c3663638ef381da1a8ea6b1814d47728
John DeHart <jdehart@alionscience.com>
2020-08-28 14:59:38 -0400

    submit

--- openPLM/mechanical.py
+++ openPLM/mechanical.py
@@ -2,10 +2,18 @@
 from django.db import models
 from django.contrib import admin
 
-from openPLM.plmapp.models import Part
-from openPLM.plmapp.controllers import PartController
-from openPLM.plmapp.utils import get_next_revision
+from django.utils.translation import ugettext_lazy as _
+
+from openPLM.plmapp.models import Part, ParentChildLinkExtension, register_PCLE
+#from openPLM.plmapp.controllers import PartController
+#from openPLM.plmapp.utils import get_next_revision
 # end of imports
+
+def register(cls):
+    try:
+        admin.site.register(cls)
+    except admin.sites.AlreadyRegistered:
+        pass
 
 # class Mechanical
 class Mechanical(Part):
@@ -16,21 +24,27 @@ class Mechanical(Part):
     details = models.TextField(blank=False)
     
     # Mechanical properties
-    @property
+    #@property
+    #def attributes(self):
+    #    attrs = list(super(Mechanical, self).attributes)
+    #    #attrs.extend(["nb_wheels", "color", "details"])
+    #    attrs.extend(["details"])
+    #    return attrs
+		
+		
+	@property
     def attributes(self):
-        attrs = list(super(Mechanical, self).attributes)
-        #attrs.extend(["nb_wheels", "color", "details"])
-        attrs.extend(["details"])
-        return attrs
+        attrs = ["details"]
+        return super(Mechanical, self).attributes + attrs
 
     # excluded_creation_fields
-    @classmethod
-    def excluded_creation_fields(cls):
-        return super(Mechanical, cls).excluded_creation_fields() + ["details"]
+    #@classmethod
+    #def excluded_creation_fields(cls):
+        #return super(Mechanical, cls).excluded_creation_fields() + ["details"]
 
 # end Mechanical
 
-admin.site.register(Mechanical)
+register(Mechanical)
 
 # class MechanicalController
 class MechanicalController(PartController):
